ff270aee9cf34069452fb886856732438561fd44
Richard Teye Lawer <richardlawer@gmail.com>
2017-07-13 14:19:35 +0000

    comments by the Ninja himself

--- person.py
+++ person.py
@@ -1,3 +1,4 @@
+# function for age and name
 def get_age(age):
   return (int)age
 
@@ -8,4 +9,7 @@ Age=raw-input("whats your age")
  print "your age is:" + " " + " " + get_age(Age)
 
 Name=raw_input("hats your name")
+
+
+# printing age and name
  print "your name is " + get_name(Name) 
